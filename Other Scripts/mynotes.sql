2d352e26a7b15c3ed323f7a40bf32ff379ee82b6
Lakshmi Chowdam <chowdam.net@gmail.com>
2019-10-18 14:45:50 -0400

    updated notes

--- Other Scripts/mynotes.sql
+++ Other Scripts/mynotes.sql
@@ -1,1 +1,46 @@
 -- SQL Snippets - quick queries here
+
+SELECT TOP (1000) [CityID]
+      ,[CityName]
+      ,[StateProvinceID]
+      ,[Location]
+      ,[LatestRecordedPopulation]
+      ,[LastEditedBy]
+  FROM [WideWorldImporters].[Application].[Cities]
+
+
+  SELECT  [CityName] ,[StateProvinceID]      
+  FROM [WideWorldImporters].[Application].[Cities]
+
+
+-- Find 10 most frequent city names
+SELECT  top (20) [CityName] , COUNT([StateProvinceID])  as Frequency
+from [WideWorldImporters].[Application].[Cities] --37940 rows affected
+GROUP BY CityName
+order by Frequency desc;
+
+select top (1000) StockItemID, StockItemName, UnitPrice, RecommendedRetailPrice
+from WideWorldImporters.Warehouse.StockItems
+where StockItemID = 224
+
+SELECT TOP (1000)  *   FROM [WideWorldImporters].[Website].[Customers]
+
+
+
+
+select compatibility_level 
+from sys.databases where name = 'WideWorldImporters'
+
+--ALTER DATABASE WideWorldImporters  
+--SET COMPATIBILITY_LEVEL = 150;  
+--GO
+
+--DBCC TRACESTATUS (809);
+
+--select SERVERPROPERTY ('IspolyBaseInstalled') as IspolyBaseInstalled
+
+---- Enable poly base
+--exec sp_configure 'polybase enabled';
+
+-- exec sp_configure @configname = 'polybase enabled', @configvalue = 1;
+-- RECONFIGURE;
